0477c334c16cbde7f80cbbe0c7cd54b038dfb13e
Marcelo Wippel <mawippel2@hotmail.com>
2019-06-13 16:40:32 -0300

    feat: creating .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,28 +2,35 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+if(!process.env.MONGO_USER || !process.env.MONGO_PASS){
+	console.log('You have to create a .env file!')
+}
+
 mongoose.connect(
-  "mongodb+srv://mawippel:mawippel@instagramfeed-xp7ip.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true
-  }
+	`mongodb+srv://${process.env.MONGO_USER}:${
+		process.env.MONGO_PASS
+	}@instagramfeed-xp7ip.mongodb.net/test?retryWrites=true&w=majority`,
+	{
+		useNewUrlParser: true
+	}
 );
 
 app.use((req, res, next) => {
-  req.io = io;
-  next();
+	req.io = io;
+	next();
 });
 app.use(cors());
 
 app.use(
-  "/files",
-  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+	"/files",
+	express.static(path.resolve(__dirname, "..", "uploads", "resized"))
 );
 app.use(require("./routes"));
 server.listen(3333);
